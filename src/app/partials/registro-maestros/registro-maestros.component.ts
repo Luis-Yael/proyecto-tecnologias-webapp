132aba116533119a465597947b601d786044d4d1
Luis Yael Mendez <luisyaelms@gmail.com>
2024-10-29 01:23:58 -0600

    Validaciones
    Se agregan validaciones en forms.

--- src/app/partials/registro-maestros/registro-maestros.component.ts
+++ src/app/partials/registro-maestros/registro-maestros.component.ts
@@ -149,4 +149,15 @@ export class RegistroMaestrosComponent implements OnInit{
     this.maestro.fecha_nacimiento = event.value.toISOString().split("T")[0];
     console.log("Fecha: ", this.maestro.fecha_nacimiento);
   }
+  public soloLetras(event: KeyboardEvent) {
+    const charCode = event.key.charCodeAt(0);
+    // Permitir solo letras (mayúsculas y minúsculas) y espacio
+    if (
+      !(charCode >= 65 && charCode <= 90) &&  // Letras mayúsculas
+      !(charCode >= 97 && charCode <= 122) && // Letras minúsculas
+      charCode !== 32                         // Espacio
+    ) {
+      event.preventDefault();
+    }
+  }
 }
